b94628662e546707609d8df8be6db845fc7e77fd
aahan-bagga <42304429+aahan-bagga@users.noreply.github.com>
2021-11-03 22:34:43 -0700

    Create pyautogui.py

---
+++ pyautogui.py
@@ -0,0 +1,39 @@
+import pyautogui as py
+#Size of monitor
+#print(py.size())
+
+#X and Y position of the mouse cursor
+#print(py.position())
+
+#Move the mouse to a certain position
+#py.moveTo(356,973)
+
+#Click the mouse
+#py.click()
+
+#Click the mouse at a certain position
+#py.click(1048,375)
+
+#Move your mouse 10 pixels down
+#py.move(10000,0)
+
+#Double click on the screen
+#py.doubleClick()
+
+#Press a certain key on your keyboard
+#py.press("tab")
+
+#Write something
+#py.write("JiffyTec", interval = 0.1)
+
+#Press a certain key down and hold it
+#py.keyDown("shift")
+
+#Let go of that key that was being held down
+#py.keyUp("shift")
+
+#Execute a specific hotkey combination
+#py.hotkey("command", "c")
+
+#Alert the user with a pop-up prompt
+#py.alert("Press ok if you love JiffyTec")
